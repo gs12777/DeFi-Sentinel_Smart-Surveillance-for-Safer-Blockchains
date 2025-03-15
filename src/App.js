ee7fefb1f08b547377983877bef09bbe4e9e2477
gurusai <gurusaisashank2004@gmail.com>
2025-03-16 02:33:00 +0530

    Removed default React text

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* ADD THIS HEADING */}
+        <h1>Blockchain Fraud Detection System</h1>
+        
+        {/* KEEP THE LOGO (OPTIONAL) */}
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
