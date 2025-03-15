6fed09f52750341eafc4db6db548c6f8a8cc7328
gurusai <gurusaisashank2004@gmail.com>
2025-03-16 03:26:35 +0530

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -5,15 +5,34 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+<<<<<<< HEAD
         {/* ADD THIS HEADING */}
         <h1>Blockchain Fraud Detection System</h1>
         
         {/* KEEP THE LOGO (OPTIONAL) */}
         <img src={logo} className="App-logo" alt="logo" />
        
+=======
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+>>>>>>> a0aee60 (Initialize project using Create React App)
       </header>
     </div>
   );
 }
 
+<<<<<<< HEAD
 export default App;
+=======
+export default App;
+>>>>>>> a0aee60 (Initialize project using Create React App)
